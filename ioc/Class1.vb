129bdf3cb043df74cd0c272a156d95384d3876e2
diegobrocchi <dbrocchi@gmail.com>
2015-03-11 18:23:45 +0100

    solo una prova

--- ioc/Class1.vb
+++ ioc/Class1.vb
@@ -2,6 +2,9 @@
     Implements IPizzaTCP
 
     Public Function SendCommand(ByVal command As String) As String Implements IPizzaTCP.SendCommand
+        Dim i As Integer
+        i = 19
+
 
     End Function
 End Class
